c98acbc0e6b53fb6b858fb06c0e59ff6e3c5734e
SBAUser <sbauser@sbausers-macbook-pro.local>
2019-09-26 14:42:59 -0700

    added more images, made buttons functional, added sound

--- Bip the Guy/ViewController.swift
+++ Bip the Guy/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: PROPERTIES
     @IBOutlet weak var imagetoPunch: UIImageView!
+    
+    // MARK: Variables
+    var audioPlayer = AVAudioPlayer()
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,16 +32,52 @@ class ViewController: UIViewController {
         
         UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options:[], animations: { self.imagetoPunch.bounds = bounds }, completion: nil)
     }
+    
+    func playSound(soundName: String, audioPlayer:  inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        } else {
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        imagetoPunch.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 
     // MARK: Actions
     @IBAction func libraryPressed(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func cameraPressed(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
       animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
     }
     
 }
