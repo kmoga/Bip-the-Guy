f73845ab3b6660082245c8e31644bb84e50a2db1
SBAUser <sbauser@sbausers-macbook-pro.local>
2019-09-26 13:46:01 -0700

    set up UI and added animation

--- Bip the Guy/ViewController.swift
+++ Bip the Guy/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    // MARK: PROPERTIES
+    @IBOutlet weak var imagetoPunch: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    // MARK: FUNCTIONS
+    
+    func animateImage() {
+        let bounds = self.imagetoPunch.bounds
+        let shrinkvalue: CGFloat = 60
+        
+        //shrink imagetoPunch by 60 px
+        self.imagetoPunch.bounds = CGRect (x: self.imagetoPunch.bounds.origin.x + shrinkvalue, y:self.imagetoPunch.bounds.origin.y + shrinkvalue, width: self.imagetoPunch.bounds.size.width - shrinkvalue, height: self.imagetoPunch.bounds.size.height - shrinkvalue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options:[], animations: { self.imagetoPunch.bounds = bounds }, completion: nil)
+    }
 
-
+    // MARK: Actions
+    @IBAction func libraryPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+      animateImage()
+    }
+    
 }
 
